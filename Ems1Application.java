279a033d6acfa5f72534c20d4923ff093f552331
psbodade26t <111677483+psbodade26t@users.noreply.github.com>
2023-01-10 12:23:07 +0530

    Add files via upload

---
+++ Ems1Application.java
@@ -0,0 +1,12 @@
+package com.greatlearning.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ems1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ems1Application.class, args);
+	}
+}
